d7f1cdd1069f8af4281e4b55e5a9eddd40f01269
Cristhian <alcantaralc3@gmail.com>
2018-02-17 00:29:16 +0100

    se elimino librerias que no aportaban nada

--- split.c
+++ split.c
@@ -1,10 +1,9 @@
 #include "persona.h"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+
 
 int main(int argc, char *argv[]){
 	

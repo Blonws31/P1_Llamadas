b80cfd53163bdd7db4f5db04dccccccaac73a890
Cristhian <alcantaralc3@gmail.com>
2018-02-17 00:34:56 +0100

    cambio total, se eliminó librerias que no aportaban nada y se usó la idea de los contadores de la version anterior, uso de lseek para leer una linea especifica del fichero, ya que se tiene que leer de atras hacia adelante, los contadores iran disminuyendo en 1 unidad

--- combine.c
+++ combine.c
@@ -1,26 +1,17 @@
 #include "persona.h"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
 
 int main(int argc, char *argv[]){
     
-    Person persona;
+    Person persona1;
     Person persona2; 
-
-    struct stat sb;
-    stat(argv[1],&sb);
-    struct stat bs;
-    stat(argv[2],&bs);
 
     int fd1 = open (argv[1], O_RDONLY);
     int fd2 = open (argv[2], O_RDONLY);
-    int fd3 = open (argv[3], O_RDWR | O_CREAT);
+    int fd3 = open (argv[3], O_RDWR | O_CREAT | O_TRUNC);
 
     if(argc!=4){
 	printf("Numero de argumentos invalidos\n");
@@ -30,56 +21,51 @@ int main(int argc, char *argv[]){
     int cont = 0;
     int cont2 = 0;
 
-    char *cadena = "\0";
-    char *cadena2 = "\0";	
-
-    char aux[(sb.st_size/sizeof(Person))][sizeof(persona.name)];
-    char aux2[(bs.st_size/sizeof(Person))][sizeof(persona.name)];
-
-    char aux3[] = "NULL";
-
-
     if (fd1 != -1){
 	if(fd2 != -1){
 
-		while(read(fd1, &persona, sizeof(Person))!= 0){	
-			cadena = persona.name;
-			strcpy(aux[cont],cadena);
+		while(read(fd1, &persona1, sizeof(Person))!= 0){	
 			cont++;
 		}
 
 		while(read(fd2, &persona2, sizeof(Person))!= 0){
-			cadena2 = persona2.name;
-			strcpy(aux2[cont2],cadena2);
 			cont2++;
 		}
+
 		int contador = 0;
-		int suma = cont+cont2-1;
-		strcpy(aux3,aux[cont-1]);
-		cont--;
+		int suma = cont+cont2;
 		while(contador < suma){
-			cont--;
-			cont2--;
-			if(cont < 0 && cont2 >= 0){
-				strcat(aux3," ");
-				strcat(aux3,aux2[cont2]);
+			if(cont <= 0 && cont2 > 0){
+				cont2--;
+				lseek(fd2,sizeof(Person)*cont2,SEEK_SET);
+				read(fd2, &persona2, sizeof(Person));
+		    		write(fd3, &persona2, sizeof(Person));
+
+			}	
+			else if(cont2 <= 0 && cont > 0){
+				cont--;
+				lseek(fd1,sizeof(Person)*cont,SEEK_SET);
+				read(fd1, &persona1, sizeof(Person));
+		    		write(fd3, &persona1, sizeof(Person));	
+
 			}
-			else if(cont2 < 0 && cont >= 0){
-				strcat(aux3," ");
-				strcat(aux3,aux[cont]);	
-			}
-			else if(cont < 0 && cont2 < 0){
+			else if(cont <= 0 && cont2 <= 0){
 				contador = suma;
 			}
 			else{
-			strcat(aux3," ");
-			strcat(aux3,aux2[cont2]);			
-			strcat(aux3," ");
-			strcat(aux3,aux[cont]);
+				cont--;
+				lseek(fd1,sizeof(Person)*cont,SEEK_SET);
+				read(fd1, &persona1, sizeof(Person));
+		    		write(fd3, &persona1, sizeof(Person));
+
+				cont2--;
+				lseek(fd2,sizeof(Person)*cont2,SEEK_SET);
+				read(fd2, &persona2, sizeof(Person));
+		    		write(fd3, &persona2, sizeof(Person));
+
 			}
 			contador++;
 		}
-		write(fd3,aux3,strlen(aux3));
 		close(fd1);
 		close(fd2);
 		close(fd3);
